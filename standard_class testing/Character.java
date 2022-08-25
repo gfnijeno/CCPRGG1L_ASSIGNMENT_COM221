4bc4e5eccb9763ece382ea19f83f5aeffcbf770b
Mureigne Leivin Y. Daet <111488534+gfnijeno@users.noreply.github.com>
2022-08-25 15:00:25 +0800

    Create Character.java

---
+++ standard_class testing/Character.java
@@ -0,0 +1,18 @@
+public class Character {
+    String introduction;
+    int age;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("hi mga beh ako si " + name);
+    }
+
+    public void sayMyIntroduction() {
+        System.out.println("ako ay " + introduction);
+    }
+
+    public void sayMyAge() {
+        System.out.println("ako ay " + age + "  gulang.");
+    }
+    
+}
