c2eef8739b89a5b227f18a67f9940c27bc628dc0
Mureigne Leivin Y. Daet <111488534+gfnijeno@users.noreply.github.com>
2022-08-25 15:00:00 +0800

    Create App.java

---
+++ standard_class testing/App.java
@@ -0,0 +1,69 @@
+public class App {
+    public static void main(String[] args) {
+        
+        Character mark = new Character();
+
+        mark.name = "mark lee";
+        mark.introduction = "leader ng nct dream";
+        mark.age = 23;
+        mark.sayMyName();
+        mark.sayMyIntroduction();
+        mark.sayMyAge();
+
+        Character renjun = new Character();
+
+        renjun.name = "huang renjun";
+        renjun.introduction = "cutie pie lang";
+        renjun.age = 22;
+        renjun.sayMyName();
+        renjun.sayMyIntroduction();
+        renjun.sayMyAge();
+
+        Character jeno = new Character();
+
+        jeno.name = "jeno lee";
+        jeno.introduction = "bf ni mureigne";
+        jeno.age = 22;
+        jeno.sayMyName();
+        jeno.sayMyIntroduction();
+        jeno.sayMyAge();
+
+        Character haechan = new Character();
+
+        haechan.name = "lee haechan";
+        haechan.introduction = "maharot";
+        haechan.age = 22;
+        haechan.sayMyName();
+        haechan.sayMyIntroduction();
+        haechan.sayMyAge();
+
+        Character jaemin = new Character();
+
+        jaemin.name = "na jaemin";
+        jaemin.introduction = "pogi so much";
+        jaemin.age = 22;
+        jaemin.sayMyName();
+        jaemin.sayMyIntroduction();
+        jaemin.sayMyAge();
+
+        Character chenle = new Character();
+
+        chenle.name = "zhong chenle";
+        chenle.introduction = "swagapino boss chenle";
+        chenle.age = 21;
+        chenle.sayMyName();
+        chenle.sayMyIntroduction();
+        chenle.sayMyAge();
+
+        Character jisung = new Character();
+
+        jisung.name = "park jisung";
+        jisung.introduction = "magaling sumayaw lagot *nagpopping*";
+        jisung.age = 20;
+        jisung.sayMyName();
+        jisung.sayMyIntroduction();
+        jisung.sayMyAge();
+    
+
+    }
+}
