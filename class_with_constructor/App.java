c343a295baf81e569dd95473fbcde5ac14008a89
Mureigne Leivin Y. Daet <111488534+gfnijeno@users.noreply.github.com>
2022-08-25 15:22:26 +0800

    Update App.java

--- class_with_constructor/App.java
+++ class_with_constructor/App.java
@@ -5,15 +5,30 @@ public class App {
         Character knight = new Character(10, 5, 0, "Chen");
         // Calls the sayMyName method
         knight.sayMyName();
+        // Calls the sayMyStrength method
+        knight.sayMyStrength();
 
         // Creates a new object named "mage" from Character class
         Character mage = new Character(0, 5, 10, "Akasha");
         // Calls the sayMyName method
         mage.sayMyName();
+        // Calls the sayMyStrength method
+        mage.sayMyStrength();
 
         // Creates a new object named "mage" from Character class
         Character thief = new Character(5, 10, 0, "Gondar");
         // Calls the sayMyName method
         thief.sayMyName();
+        // Calls the sayMyStrength method
+        thief.sayMyStrength();
+
+        // Creates a new object named "ärcher" from Character class
+        Character archer = new Character(10, 6, 9, "Jeno");
+        archer.sayMyName();
+        archer.sayMyStrength();
+
+        Character healer = new Character(2, 4, 10, "Mark");
+        healer.sayMyName();
+        healer.sayMyStrength();
     }
 }
